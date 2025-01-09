25605437dbcbc3fed9d7b0bd9b64025c73793d68
Arash zandi <ZandiArash@Hotmail.com>
2025-01-09 13:40:19 +0330

    FirstInit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // app.MapOpenApi();
+    app.UseSwagger(); // Enable the Swagger JSON endpoint
+    app.UseSwaggerUI(); // Enable the Swagger UI
+    // app.UseSwaggerUI(options =>
+    // {
+    //     options.SwaggerEndpoint("/openapi/v1.json", "Your Custom Title");
+    // });
+
+    app.MapScalarApiReference();
+    // app.MapScalarApiReference(options =>
+    // {
+    //     options
+    //     .WithTitle("Your Custom Title")
+    //     .WithTheme(ScalarTheme.Mars)
+    //     .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+    // });
+
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
